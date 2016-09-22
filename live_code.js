8e8dbb0dac419c5d9df7e8c7d96dd9cbe46372ed
nathanallen <thenathanator@gmail.com>
2016-09-22 15:47:41 -0700

    setup package.json and .gitignore node_modules

--- live_code.js
+++ live_code.js
@@ -1,1 +1,10 @@
-// HELLO WORLD!
+// HELLO WDI32!
+
+// NPM
+// # make `package.json` file
+// > echo '{}' > package.json
+// > npm install --save express
+// > npm install --save mongoose
+
+// # Git Ignore node_modules folder:
+// > echo 'node_modules' > .gitignore
