8f3c431761407dac84e2c78822d6d8fac5cbaa24
nathanallen <thenathanator@gmail.com>
2016-09-22 16:02:27 -0700

    setup mongoose and User model

--- live_code.js
+++ live_code.js
@@ -8,3 +8,18 @@
 
 // # Git Ignore node_modules folder:
 // > echo 'node_modules' > .gitignore
+
+// Database
+var mongoose = require('mongoose');
+
+// Model
+var userSchema = new mongoose.Schema({
+  name: String,
+  username: {
+    type: String,
+    unique: true // uniqueness constraint
+  },
+  password_digest: String
+});
+
+var User = mongoose.model("User", userSchema);
